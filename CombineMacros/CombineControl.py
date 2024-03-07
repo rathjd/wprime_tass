c1f78b8e97128e3289ab2455576d244f06b63aa1
Denis Rathjens <Denis.Rathjens@Cern.ch>
2024-03-07 19:09:10 +0100

    new combine card builder

--- CombineMacros/CombineControl.py
+++ CombineMacros/CombineControl.py
@@ -4,7 +4,7 @@ import ROOT
 #define the input names
 signalNames = ["M$MASS"] #$MASS gets replaced by runing combine with -m option, supports only 1 signal mass at a time
 bgrNames = ["ttbar", "wjets", "single_top", "diboson"]
-binNumber = 1153 #this needs to be set
+binNumber = 1153 #this needs to be set, valid entries are 1150, 1160, 2150, 2160
 yearNumber = 2018
 
 try:
@@ -23,140 +23,179 @@ except:
 
 
 
+binString = str(binNumber)
 yearName = str(yearNumber)
-binName = "Wprime" + str(binNumber)
 
 #First, generate the shape variation histograms
 print("Starting processing of intermediate files")
-#if os.path.isdir("TestHistograms"):
-#  os.system("rm TestHistograms/*.root") #reset 
-#else:
-#  os.system("mkdir TestHistograms") #make directory, if it doesn't exist
-os.system("root -l -b -q 'runCombineHistogramDumpster.C+(" + str(binNumber) + ", " + str(yearNumber) + ")'") #This does not yet provide a year, as only 2018 is processed
-os.system("hadd -f SimpleShapes_" + binName + ".root TestHistograms/SimpleShapes_Bin" + str(binNumber) + "*.root") #hadd all histograms to a convenient combined file
-os.system("hadd -f HT_SimpleShapes_" + binName + ".root TestHistograms/HT_SimpleShapes_Bin" + str(binNumber) + "*.root") #hadd all histograms to a convenient combined file
-os.system("hadd -f TwoD_SimpleShapes_" + binName + ".root TestHistograms/TwoD_SimpleShapes_Bin" + str(binNumber) + "*.root") #hadd all histograms to a convenient combined file
+if os.path.isdir("TestHistograms"):
+  #os.system("rm TestHistograms/*.root") #reset
+  print("already exists")
+else:
+  os.system("mkdir TestHistograms") #make directory, if it doesn't exist
 
-#define correlated entities for usage in card
-#https://twiki.cern.ch/twiki/bin/view/CMS/LumiRecommendationsRun2#Combination_and_correlations
-lumiCorrVal = "0.0"
-lumiStatVal = "0.0"
-if yearNumber == 2016:
-  lumiCorrVal = "1.006"
-  lumiStatVal = "1.01"
-if yearNumber == 2017:
-  lumiCorrVal = "1.009"
-  lumiStatVal = "1.02"
-if yearNumber == 2018:
-  lumiCorrVal = "1.02"
-  lumiStatVal = "1.015"
+#run dump from intermediate tree including ST correction cycle, generates 3- and 4-tag regions both
+os.system("root -l -b -q 'runCombineHistogramDumpster.C+(" + str(binNumber) + ", " + yearName + ")'")
 
-#define all the systematic names, types, and values
-systNames = ["lumiCorr",  "lumiStat"+yearName, "electron", "muonTrigger", "muonId", "muonIso", "BjetTagCorr", "BjetTagUncorr"+yearName, "PUID",  "L1PreFiring", "PUreweight", "PDF",   "LHEScale", "electronScale", "electronRes", "JES",   "JER"]
-systTypes = ["lnN",       "lnN",               "shape",    "shape",       "shape",  "shape",   "shape",       "shape",                  "shape", "shape",       "shape",      "shape", "shape",    "shape",         "shape",       "shape", "shape"] 
-systVals  = [lumiCorrVal, lumiStatVal,         "1",        "1",           "1",      "1",       "1",           "1",                      "1",     "1",           "1",          "1",     "1",        "1",             "1",           "1",     "1"]
+bins = [binString[0:3]+"1", binString[0:3]+"2", binString[0:3]+"3", binString[0:3]+"4"]
 
-#write the actual combine card
-print("Creating Combine card file")
-f = open("Test_" + binName + ".txt","w")
-f.write("imax " + str(1) + "\n") #number of channels
-f.write("jmax " + str(len(bgrNames)) + "\n") #number of backgrounds
-f.write("kmax " + str(len(systNames)) + "\n") #number of nuisance parameters
-f.write("----------\n")
-f.write("shapes * * SimpleShapes_" + binName + ".root $PROCESS_$CHANNEL_ $PROCESS_$CHANNEL_$SYSTEMATIC\n")
-f.write("----------\n")
-f.write("bin         " + binName + "\n")
+for binN in bins:
+  fileName = binN + "_" + yearName
+  binName = "Wprime" + binN + "_" + yearName
 
-#load ROOT file, find observation number
-print("Reading observed events numbers")
-r = ROOT.TFile.Open("SimpleShapes_" + binName + ".root", "read")
-h = r.Get("data_obs_" + binName + "_")
-observed = h.Integral()
-f.write("observation " + str(observed) + "\n")
-f.write("----------\n")
+  if os.path.isdir(fileName):
+      print(fileName," directory already exists, removing it")
+      os.system("rm -rf fileName")
+  os.system("mkdir " + fileName)
 
-##assemble strings for lines
-print("Assembling lines for Combine card")
-#sysetmatic lines first, as this is an entire column to be processed, later, and they are longest
-systLines = []
-maxLength = 0
-for i in range(0, len(systNames)): #assemble systematic names
-  systLines.append(systNames[i])
-  maxLength = max(maxLength, len(systLines[i]))
+  #assemble results
+  if yearNumber != 2016:
+    os.system("hadd -f " + fileName + "/SimpleShapes_" + binName + ".root TestHistograms/SimpleShapes_Bin" + fileName + "_*.root") #hadd all histograms to a convenient combined file
+    os.system("hadd -f " + fileName + "/HT_SimpleShapes_" + binName + ".root TestHistograms/HT_SimpleShapes_Bin" + fileName + "_*.root") #hadd all histograms to a convenient combined file
+    os.system("hadd -f " + fileName + "/TwoD_SimpleShapes_" + binName + ".root TestHistograms/TwoD_SimpleShapes_Bin" + fileName + "_*.root") #hadd all histograms to a convenient combined file 
+  else: #2016 has to be merged with 2016apv, which is handled by this exception
+    fileNameAlt = fileName + "apv"
+    os.system("hadd -f " + fileName + "/SimpleShapes_" + binName + ".root TestHistograms/SimpleShapes_Bin" + fileName + "_*.root TestHistograms/SimpleShapes_Bin" + fileNameAlt + "_*.root") #hadd all histograms to a convenient combined file
+    os.system("hadd -f " + fileName + "/HT_SimpleShapes_" + binName + ".root TestHistograms/HT_SimpleShapes_Bin" + fileName + "_*.root TestHistograms/HT_SimpleShapes_Bin" + fileNameAlt + "_*.root") #hadd all histograms to a convenient combined file
+    os.system("hadd -f " + fileName + "/TwoD_SimpleShapes_" + binName + ".root TestHistograms/TwoD_SimpleShapes_Bin" + fileName + "_*.root TestHistograms/TwoD_SimpleShapes_Bin" + fileNameAlt + "_*.root") #hadd all histograms to a convenient combined file
 
-maxLength2 = 0
-for i in range(0, len(systLines)): #align systematic names, then assemble systematic types
-  while len(systLines[i]) < (maxLength + 3):
-    systLines[i] += " "
-  systLines[i] += systTypes[i]
-  maxLength2 = max(maxLength2, len(systLines[i]))
+  #transfer SF files where appropriate
+  if int(binN) % 10 == 2:
+      os.system("cp TestHistograms/SF_Bin1151_2016.root " + fileName + "/.")
+  elif int(binN) % 10 > 2:
+      os.system("cp TestHistograms/SF_Bin1152_2016.root " + fileName + "/.")
 
-for i in range(0, len(systLines)): #align syst types
-  while len(systLines[i]) < (maxLength2 + 5):
-    systLines[i] += " "
+  #skip making cards for 1- and 2-tag regions
+  if int(binN) % 10 < 3:
+      continue
 
-maxLength2 = len(systLines[0])
+  #define correlated entities for usage in card
+  #https://twiki.cern.ch/twiki/bin/view/CMS/LumiRecommendationsRun2#Combination_and_correlations is now a shape uncertainty built into histograms in the file
 
-#assemble bin and process block
-allNames = signalNames + bgrNames
-allNumbers = []
-for i in range(-len(signalNames)+1, 1): #negative and zero numbers for signals
-  allNumbers.append(i)
-for i in range (1, len(bgrNames)+1): #positive nonzero numbers for backgrounds
-  allNumbers.append(i)
-binLine      = "bin     "
-processLine1 = "process "
-processLine2 = "process "
-rateLine     = "rate    "
+  #define all the systematic names, types, and values
+  systNames = ["lumiCorr", "lumiStat"+yearName, "electron"+yearName, "muonTrigger"+yearName, "muonId"+yearName, "muonIso"+yearName, "BjetTagCorr", "BjetTagUncorr"+yearName, "PUID"+yearName, "L1PreFiring"+yearName, "PUreweight"+yearName, "PDF",   "LHEScale", "electronScale"+yearName, "electronRes"+yearName, "JES"+yearName, "JER"+yearName, "STfit_"+yearName+"_"+binString[0:3]+"2", "NLLnonClosure"+yearName+"_"+binString[0:3]+"2"]
+  systTypes = ["shape",    "shape",             "shape",             "shape",                "shape",           "shape",            "shape",       "shape",                  "shape",         "shape",                "shape",               "shape", "shape",    "shape",                  "shape",                "shape",        "shape",        "shape",                                  "lnN"] 
+  systVals  = ["1",        "1",                 "1",                 "1",                    "1",               "1",                "1",           "1",                      "1",             "1",                    "1",                   "1",     "1",        "1",                      "1",                    "1",            "1",            "0",                                      "0"]
 
-#align bin, process, and rate lines with systematic line length
-while len(binLine) < maxLength2:
-  binLine += " "
-while len(processLine1) < maxLength2:
-  processLine1 += " "
-while len(processLine2) < maxLength2:
-  processLine2 += " "
-while len(rateLine) < maxLength2:
-  rateLine += " "
+  #write the actual combine cards
+  CardNames = [["FitMass", ""], ["HT", "HT_"]] #by default makes both the fit mass and HT extraction cards, separately
+  for CardName in CardNames:
+    print("Creating Combine card file")
+    f = open(fileName + "/" + CardName[0] + "_" + binName + ".txt","w")
+    f.write("imax " + str(1) + "\n") #number of channels
+    f.write("jmax " + str(len(bgrNames)) + "\n") #number of backgrounds
+    f.write("kmax " + str(len(systNames)) + "\n") #number of nuisance parameters
+    f.write("----------\n")
+    f.write("shapes * * " + CardName[1] + "SimpleShapes_" + binName + ".root $PROCESS_$CHANNEL_ $PROCESS_$CHANNEL_$SYSTEMATIC\n")
+    f.write("----------\n")
+    f.write("bin         " + binName + "\n")
 
-for i in range(0, len(allNames)): #assemble bin, process, rate, and systematic line entries, then align
-  binLine += binName
-  processLine1 += allNames[i]
-  processLine2 += str(allNumbers[i])
-  rateLine += "-1" #this option makes Combine read the rate from the histogram integrals
+    #load ROOT file, find observation number
+    print("Reading observed events numbers")
+    r = ROOT.TFile.Open(fileName + "/" + CardName[1] + "SimpleShapes_" + binName + ".root", "read")
+    h = r.Get(CardName[1] + "data_obs_" + binName + "_")
+    observed = h.Integral(0,-1)
+    f.write("observation " + str(observed) + "\n")
+    f.write("----------\n")
 
-  currentLength = max(len(binLine), len(processLine1), len(processLine2), len(rateLine))
+    ##assemble strings for lines
+    print("Assembling lines for Combine card ",CardName[0])
+    #systematic lines first, as this is an entire column to be processed, later, and they are longest
+    systLines = []
+    maxLength = 0
+    for i in range(0, len(systNames)): #assemble systematic names
+      systLines.append(systNames[i])
+      maxLength = max(maxLength, len(systLines[i]))
+
+    maxLength2 = 0
+    for i in range(0, len(systLines)): #align systematic names, then assemble systematic types
+      while len(systLines[i]) < (maxLength + 3):
+        systLines[i] += " "
+      systLines[i] += systTypes[i]
+      maxLength2 = max(maxLength2, len(systLines[i]))
+
+    for i in range(0, len(systLines)): #align syst types
+      while len(systLines[i]) < (maxLength2 + 5):
+        systLines[i] += " "
+
+    maxLength2 = len(systLines[0])
+
+    #assemble bin and process block
+    allNames = signalNames + bgrNames
+    allNumbers = []
+    for i in range(-len(signalNames)+1, 1): #negative and zero numbers for signals
+      allNumbers.append(i)
+    for i in range (1, len(bgrNames)+1): #positive nonzero numbers for backgrounds
+      allNumbers.append(i)
+    binLine      = "bin     "
+    processLine1 = "process "
+    processLine2 = "process "
+    rateLine     = "rate    "
+
+    #align bin, process, and rate lines with systematic line length
+    while len(binLine) < maxLength2:
+      binLine += " "
+    while len(processLine1) < maxLength2:
+      processLine1 += " "
+    while len(processLine2) < maxLength2:
+      processLine2 += " "
+    while len(rateLine) < maxLength2:
+      rateLine += " "
+
+    for i in range(0, len(allNames)): #assemble bin, process, rate, and systematic line entries, then align
+      binLine += binName
+      processLine1 += allNames[i]
+      processLine2 += str(allNumbers[i])
+      rateLine += "-1" #this option makes Combine read the rate from the histogram integrals
+
+      currentLength = max(len(binLine), len(processLine1), len(processLine2), len(rateLine))
   
-  for j in range(0, len(systLines)): #assemble systematic values
-    systLines[j] += systVals[j]
-    currentLength = max(currentLength, len(systLines[j]))
+      for j in range(0, len(systLines)): #assemble systematic values
+        if allNames[i] == "ttbar" and systLines[j].find("STfit") > -1:
+          systLines[j] += systVals[j].replace("0","1") #activate ST fit uncertainty for ttbar only in the card
+        elif allNames[i] != "M$MASS" and systLines[j].find("NLLnonClosure") > -1: #NLL non-closure systematic for all backgrounds
+          NLLresF = ROOT.TFile.Open(fileName + "/" + CardName[1] + "SF_Bin" + binName[6:9] + "2" +binName[10:] + ".root", "read")
+          NLLresH = NLLresF.Get("NLLresidual_" + binName[6:9] + "2" + binName[10:])
+          NLLH    = r.Get("NegLogLnoB_" + allNames[i] + "_" + binName + "_")
+          NLLresH.Multiply(NLLH)
+          if NLLH.Integral() == 0:
+            continue
+          ratio = str(NLLresH.Integral()/NLLH.Integral())
+          dot = ratio.find(".")
+          if dot >= 0:
+            systLines[j] += systVals[j].replace("0",ratio[0:dot+3]) #limit precision to keep cards readable
+          else:
+            systLines[j] += systVals[j].replace("0",ratio)
+        else:
+          systLines[j] += systVals[j]
+        currentLength = max(currentLength, len(systLines[j]))
 
-  #align all lines with extra space
-  currentLength += 5
+      #align all lines with extra space
+      currentLength += 5
 
-  while len(binLine) < currentLength:
-    binLine += " "
+      while len(binLine) < currentLength:
+        binLine += " "
 
-  while len(processLine1) < currentLength:
-    processLine1 += " "
+      while len(processLine1) < currentLength:
+        processLine1 += " "
 
-  while len(processLine2) < currentLength:
-    processLine2 += " "
+      while len(processLine2) < currentLength:
+        processLine2 += " "
 
-  while len(rateLine) < currentLength:
-    rateLine += " "
+      while len(rateLine) < currentLength:
+        rateLine += " "
 
-  for j in range(0, len(systLines)):
-    while len(systLines[j]) < currentLength:
-      systLines[j] += " "
+      for j in range(0, len(systLines)):
+        while len(systLines[j]) < currentLength:
+          systLines[j] += " "
 
-print("Writing Combine Card values")
-f.write(binLine + "\n")
-f.write(processLine1 + "\n")
-f.write(processLine2 + "\n")
-f.write(rateLine + "\n")
-f.write("----------\n")
+    print("Writing Combine Card values")
+    f.write(binLine + "\n")
+    f.write(processLine1 + "\n")
+    f.write(processLine2 + "\n")
+    f.write(rateLine + "\n")
+    f.write("----------\n")
 
-for i in range(0, len(systLines)):
-  f.write(systLines[i] + "\n")
+    for i in range(0, len(systLines)):
+      f.write(systLines[i] + "\n")
 
