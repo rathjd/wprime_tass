6225b60ed1f4b7539098b4c5ac5da1b376c8de33
Denis Rathjens <Denis.Rathjens@Cern.ch>
2024-09-23 18:42:43 +0200

    new mass-dependent optimizations and fixes to limit scripts

--- CombineMacros/BrazilOpt.py
+++ CombineMacros/BrazilOpt.py
@@ -4,6 +4,8 @@ from array import array
 
 savefile = TFile("CombinationAll/OptimizedLimit.root","RECREATE");
 
+b2gpath = "/eos/cms/store/group/phys_b2g/wprime/PatrickCombine/" 
+
 masses = array( 'd' )
 
 crossSections = [683.8+708.3, 321.7+336.1, 161.1+165.3, 85.92+85.82, 48.84+47.47, 29.81+27.73, 18.33+16.49, 11.73+10.25, 7.683+6.546]
@@ -15,7 +17,7 @@ binS  = [["1153", "2153"], ["1163", "2163"], ["1164", "2164"]]
 
 optimization = []
 
-compromise = True
+compromise = False
 folder = "test"
 
 #set up the optimized working points for each masspoint
@@ -44,15 +46,25 @@ if not compromise:
     #                [[7, 30], [5, 15], [1, 22]]]
     folder = "allYears_OptimizedSlices"
     #mass-dependent 2017 only optimization result
-    optimization = [[[5, 19],  [1, 9],  [4, 13]],
-                    [[6, 26],  [9, 13], [6, 13]],
-                    [[12, 27], [5, 16], [4, 13]],
-                    [[5, 30],  [6, 21], [6, 14]],
-                    [[5, 29],  [7, 21], [15, 21]],
-                    [[14, 47], [3, 14], [13, 22]],
-                    [[6, 33],  [7, 17], [13, 22]],
-                    [[4, 28],  [10, 22], [7, 22]],
-                    [[4, 28],  [1, 17], [3, 22]]]
+    #optimization = [[[5, 19],  [1, 9],  [4, 13]],
+    #                [[6, 26],  [9, 13], [6, 13]],
+    #                [[12, 27], [5, 16], [4, 13]],
+    #                [[5, 30],  [6, 21], [6, 14]],
+    #                [[5, 29],  [7, 21], [15, 21]],
+    #                [[14, 47], [3, 14], [13, 22]],
+    #                [[6, 33],  [7, 17], [13, 22]],
+    #                [[4, 28],  [10, 22], [7, 22]],
+    #                [[4, 28],  [1, 17], [3, 22]]]
+    #all year all mass-dependent optimization result
+    optimization = [[[ 5, 15], [12, 14], [12, 22]],
+                    [[ 9, 23], [ 4, 13], [12, 22]],
+                    [[ 8, 22], [ 3, 13], [12, 22]],
+                    [[10, 26], [ 4, 13], [11, 22]],
+                    [[ 3, 23], [ 4, 14], [11, 22]],
+                    [[ 9, 27], [ 4, 15], [10, 22]],
+                    [[16, 27], [ 3, 14], [12, 22]],
+                    [[14, 28], [ 7, 14], [10, 22]],
+                    [[17, 27], [ 7, 14], [10, 22]]]
 else:
 #compromise version
     optimization = [[[7, 28], [5, 13], [6, 22]],
@@ -74,7 +86,7 @@ print(folder)
 
 os.chdir("CombinationAll")
 
-for mass in range(0,8):
+for mass in range(0,9):
     masses.append(float((3+mass)*100))
     massString = str((3+mass)*100)
     
@@ -83,7 +95,7 @@ for mass in range(0,8):
     for binN in binS:
         for year in years:
             for binX in binN:
-                os.system("python3 ../SliceHTvsFitMass.py "+year+" "+binX+" "+str(optimization[mass][counterJet][0])+" "+str(optimization[mass][counterJet][1]))#+" direct")
+                os.system("python3 ../SliceHTvsFitMass.py "+year+" "+binX+" "+str(optimization[mass][counterJet][0])+" "+str(optimization[mass][counterJet][1])+" "+massString)#+" direct")
         counterJet+=1
 
     #combine all cards, first into the optimized subsets
@@ -95,14 +107,14 @@ for mass in range(0,8):
     os.system("combineCards.py CombinationSlices_Wprimex153_all_M"+massString+".txt CombinationSlices_Wprimex163_all_M"+massString+".txt CombinationSlices_Wprimex164_all_M"+massString+".txt > Optimized_WprimeAll_all_M"+massString+".txt")
 
     #make a folder to copy this masspoints interpretation files to in totality
-    if not os.path.isdir("/eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString):
-        os.system("mkdir /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString)
+    if not os.path.isdir(b2gpath+folder+"/"+massString):
+        os.system("mkdir "+b2gpath+folder+"/"+massString)
     else:
-        os.system("rm -rf /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString)
-        os.system("mkdir /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString)
+        os.system("rm -rf "+b2gpath+folder+"/"+massString)
+        os.system("mkdir "+b2gpath+folder+"/"+massString)
 
-    print("card ",os.path.exists("Optimized_WprimeAll_all._M"+massString+"txt"))
-    card = open("Optimized_WprimeAll_all._M"+massString+"txt","r")
+    print("card ",os.path.exists("Optimized_WprimeAll_all_M"+massString+".txt"))
+    card = open("Optimized_WprimeAll_all_M"+massString+".txt","r")
     lines = card.readlines()
     rootfiles = []
     for line in lines:
@@ -113,22 +125,22 @@ for mass in range(0,8):
 
     for rootfile in rootfiles:
         print(rootfile,os.path.exists(rootfile))
-        print("cp "+rootfile+" /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString+"/.")
-        os.system("cp "+rootfile+" /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString+"/.")
-        print(os.path.exists("/eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString+"/"+rootfile))
+        print("cp "+rootfile+" "+b2gpath+folder+"/"+massString+"/.")
+        os.system("cp "+rootfile+" "+b2gpath+folder+"/"+massString+"/.")
+        print(os.path.exists(b2gpath+folder+"/"+massString+"/"+rootfile))
 
     #copy current cards and all root files in final card
-    print("cp CombinationSlices_Wprime1153_2016_M"+massString+".txt CombinationSlices_Wprime1153_2017_M"+massString+".txt CombinationSlices_Wprime1153_2018_M"+massString+".txt CombinationSlices_Wprime2153_2016_M"+massString+".txt CombinationSlices_Wprime2153_2017_M"+massString+".txt CombinationSlices_Wprime2153_2018_M"+massString+".txt CombinationSlices_Wprimex153_all_M"+massString+".txt CombinationSlices_Wprime1163_2016_M"+massString+".txt CombinationSlices_Wprime1163_2017_M"+massString+".txt CombinationSlices_Wprime1163_2018_M"+massString+".txt CombinationSlices_Wprime2163_2016_M"+massString+".txt CombinationSlices_Wprime2163_2017_M"+massString+".txt CombinationSlices_Wprime2163_2018_M"+massString+".txt CombinationSlices_Wprimex163_all_M"+massString+".txt CombinationSlices_Wprime1164_2016_M"+massString+".txt CombinationSlices_Wprime1164_2017_M"+massString+".txt CombinationSlices_Wprime1164_2018_M"+massString+".txt CombinationSlices_Wprime2164_2016_M"+massString+".txt CombinationSlices_Wprime2164_2017_M"+massString+".txt CombinationSlices_Wprime2164_2018_M"+massString+".txt CombinationSlices_Wprimex164_all_M"+massString+".txt Optimized_WprimeAll_all_M"+massString+".txt /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString+"/.")
-    os.system("cp CombinationSlices_Wprime1153_2016_M"+massString+".txt CombinationSlices_Wprime1153_2017_M"+massString+".txt CombinationSlices_Wprime1153_2018_M"+massString+".txt CombinationSlices_Wprime2153_2016_M"+massString+".txt CombinationSlices_Wprime2153_2017_M"+massString+".txt CombinationSlices_Wprime2153_2018_M"+massString+".txt CombinationSlices_Wprimex153_all_M"+massString+".txt CombinationSlices_Wprime1163_2016_M"+massString+".txt CombinationSlices_Wprime1163_2017_M"+massString+".txt CombinationSlices_Wprime1163_2018_M"+massString+".txt CombinationSlices_Wprime2163_2016_M"+massString+".txt CombinationSlices_Wprime2163_2017_M"+massString+".txt CombinationSlices_Wprime2163_2018_M"+massString+".txt CombinationSlices_Wprimex163_all_M"+massString+".txt CombinationSlices_Wprime1164_2016_M"+massString+".txt CombinationSlices_Wprime1164_2017_M"+massString+".txt CombinationSlices_Wprime1164_2018_M"+massString+".txt CombinationSlices_Wprime2164_2016_M"+massString+".txt CombinationSlices_Wprime2164_2017_M"+massString+".txt CombinationSlices_Wprime2164_2018_M"+massString+".txt CombinationSlices_Wprimex164_all_M"+massString+".txt Optimized_WprimeAll_all_M"+massString+".txt /eos/cms/store/group/phys_b2g/wprime/PatrickCombine/"+folder+"/"+massString+"/.")
+    print("cp CombinationSlices_Wprime1153_2016_M"+massString+".txt CombinationSlices_Wprime1153_2017_M"+massString+".txt CombinationSlices_Wprime1153_2018_M"+massString+".txt CombinationSlices_Wprime2153_2016_M"+massString+".txt CombinationSlices_Wprime2153_2017_M"+massString+".txt CombinationSlices_Wprime2153_2018_M"+massString+".txt CombinationSlices_Wprimex153_all_M"+massString+".txt CombinationSlices_Wprime1163_2016_M"+massString+".txt CombinationSlices_Wprime1163_2017_M"+massString+".txt CombinationSlices_Wprime1163_2018_M"+massString+".txt CombinationSlices_Wprime2163_2016_M"+massString+".txt CombinationSlices_Wprime2163_2017_M"+massString+".txt CombinationSlices_Wprime2163_2018_M"+massString+".txt CombinationSlices_Wprimex163_all_M"+massString+".txt CombinationSlices_Wprime1164_2016_M"+massString+".txt CombinationSlices_Wprime1164_2017_M"+massString+".txt CombinationSlices_Wprime1164_2018_M"+massString+".txt CombinationSlices_Wprime2164_2016_M"+massString+".txt CombinationSlices_Wprime2164_2017_M"+massString+".txt CombinationSlices_Wprime2164_2018_M"+massString+".txt CombinationSlices_Wprimex164_all_M"+massString+".txt Optimized_WprimeAll_all_M"+massString+".txt "+b2gpath+folder+"/"+massString+"/.")
+    os.system("cp CombinationSlices_Wprime1153_2016_M"+massString+".txt CombinationSlices_Wprime1153_2017_M"+massString+".txt CombinationSlices_Wprime1153_2018_M"+massString+".txt CombinationSlices_Wprime2153_2016_M"+massString+".txt CombinationSlices_Wprime2153_2017_M"+massString+".txt CombinationSlices_Wprime2153_2018_M"+massString+".txt CombinationSlices_Wprimex153_all_M"+massString+".txt CombinationSlices_Wprime1163_2016_M"+massString+".txt CombinationSlices_Wprime1163_2017_M"+massString+".txt CombinationSlices_Wprime1163_2018_M"+massString+".txt CombinationSlices_Wprime2163_2016_M"+massString+".txt CombinationSlices_Wprime2163_2017_M"+massString+".txt CombinationSlices_Wprime2163_2018_M"+massString+".txt CombinationSlices_Wprimex163_all_M"+massString+".txt CombinationSlices_Wprime1164_2016_M"+massString+".txt CombinationSlices_Wprime1164_2017_M"+massString+".txt CombinationSlices_Wprime1164_2018_M"+massString+".txt CombinationSlices_Wprime2164_2016_M"+massString+".txt CombinationSlices_Wprime2164_2017_M"+massString+".txt CombinationSlices_Wprime2164_2018_M"+massString+".txt CombinationSlices_Wprimex164_all_M"+massString+".txt Optimized_WprimeAll_all_M"+massString+".txt "+b2gpath+folder+"/"+massString+"/.")
 
     #run Combine for this set of cards
-    os.system("combine -M AsymptoticLimits Optimized_WprimeAll_all_M"+massString+".txt")
+    os.system("combine -M AsymptoticLimits -m "+massString+" Optimized_WprimeAll_all_M"+massString+".txt")
 
     infile = TFile("higgsCombineTest.AsymptoticLimits.mH"+massString+".root","READ")
 
-    print("cp higgsCombineTest.AsymptoticLimits.mH"+massString+".root /eos/cms/store/group/phys_b2g/wprime/SifuCombine/"+folder+"/"+massString+"/.")
-    os.system("cp higgsCombineTest.AsymptoticLimits.mH"+massString+".root /eos/cms/store/group/phys_b2g/wprime/SifuCombine/"+folder+"/"+massString+"/.")
-    print(os.path.exists("/eos/cms/store/group/phys_b2g/wprime/SifuCombine/"+folder+"/"+massString+"/higgsCombineTest.AsymptoticLimits.mH"+massString+".root"))
+    print("cp higgsCombineTest.AsymptoticLimits.mH"+massString+".root "+b2gpath+folder+"/"+massString+"/.")
+    os.system("cp higgsCombineTest.AsymptoticLimits.mH"+massString+".root "+b2gpath+folder+"/"+massString+"/.")
+    print(os.path.exists(b2gpath+folder+"/"+massString+"/higgsCombineTest.AsymptoticLimits.mH"+massString+".root"))
 
     limitNumbers.append([])
 
@@ -144,13 +156,13 @@ TwoSigmaBand = array( 'd' )
 OneSigmaBand = array( 'd' )
 central = array( 'd' )
 massBand = array( 'd' )
-for x in range(0,8):
+for x in range(0,9):
     central.append(limitNumbers[x][2])
     OneSigmaBand.append(limitNumbers[x][1])
     TwoSigmaBand.append(limitNumbers[x][0])
     massBand.append(float((3+x)*100))
 
-for x in range(0,8):
+for x in range(0,9):
     OneSigmaBand.append(limitNumbers[8-x][3])
     TwoSigmaBand.append(limitNumbers[8-x][4])
     massBand.append(float((11-x)*100))
@@ -185,6 +197,6 @@ savefile.cd()
 canvas.Write()
 canvas.SaveAs("FullOptimizationFullCombination.pdf")
 canvas.SaveAs("FullOptimizationFullCombination.root")
-os.system("cp FullOptimizationFullCombination.pdf /eos/cms/store/group/phys_b2g/wprime/SifuCombine/"+folder+"/.")
-os.system("cp FullOptimizationFullCombination.root /eos/cms/store/group/phys_b2g/wprime/SifuCombine/"+folder+"/.")
+os.system("cp FullOptimizationFullCombination.pdf "+b2gpath+folder+"/.")
+os.system("cp FullOptimizationFullCombination.root "+b2gpath+folder+"/.")
 savefile.Close()
