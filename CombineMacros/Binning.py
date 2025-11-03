a068da14733bb3ba024f737e753f391d98827051
Denis Rathjens <Denis.Rathjens@Cern.ch>
2025-11-03 19:44:38 +0100

    new binning macro amended for dealing with duplicate quantile outputs with limited precision and new logic to demand at least 9 weighted events per bin, changed to 2017 all ttbar samples

--- CombineMacros/Binning.py
+++ CombineMacros/Binning.py
@@ -3,8 +3,23 @@ from ROOT import TH1F, TChain, TMath
 import numpy as np
 import math
 
+#Optimization of NLL cuts October 2025 version pre-ST cut after b tag weight fix
+Optimization =       [[[13, 61], [11, 24]],
+                      [[ 7, 59], [ 9, 22]],
+                      [[ 8, 65], [ 7, 24]],
+                      [[10, 76], [ 7, 24]],
+                      [[11, 75], [ 7, 24]],
+                      [[12, 67], [ 7, 24]],
+                      [[12, 67], [ 7, 24]],
+                      [[12, 67], [ 7, 24]],
+                      [[12, 74], [ 7, 24]]]
+
+NLLlimits53_500 = [0.0, 1.3, 1.7, 2.0, 2.3, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.5, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.0, 7.2, 7.4, 7.6, 7.8, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.8, 10.0, 10.2, 10.4, 10.6, 10.8, 11.0, 11.2, 11.4, 11.6, 11.8, 12.0, 12.2, 12.4, 12.6, 12.8, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.2, 14.4, 14.6, 14.8, 15.0, 15.2, 15.4, 15.6, 15.8, 16.0, 16.2, 16.4, 16.6, 16.8, 29.9]
+NLLlimits63_500 = [0.0, 0.8, 1.0, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.8, 11.1, 11.6, 12.1, 12.7, 13.6, 15.0, 29.9]
+NLLlimits64_500 = [0.2, 1.0, 1.6, 2.2, 2.8, 3.4, 4.0, 4.5, 4.8, 5.0, 5.3, 5.6, 5.9, 6.2, 6.5, 6.8, 7.2, 7.8, 8.4, 9.0, 9.6, 10.2, 10.8, 11.5, 13.0, 29.9]
+
 #function for determining the binning from desired minimum unweighted stats and input histogram in fine binning, as well as desired precision
-def determineQuantiles(hist, nStatMin, precision):
+def determineQuantiles(hist, nStatMin, precision, wHist):
     #determine desired number of quantiles, initiate empty quantiles lists
     nq = math.floor(hist.Integral()/nStatMin)
     x = []
@@ -25,30 +40,33 @@ def determineQuantiles(hist, nStatMin, precision):
     #print("target",nq,"bins")
 
     #round all elements in list to desired precision
+    Ay_clean = []
     for i in range(0, len(Ay)):
-        Ay[i] = round(Ay[i], precision)
+        member = round(Ay[i], precision)
+        if member not in Ay_clean:
+            Ay_clean.append(member)
 
-    #print(Ay)
+    print(Ay_clean,"bare cleaned quantiles")
 
     #find smallest bin size
     minimumIndex = -1
     minimum = 999.
-    for i in range(0, len(Ay)-1):
-        size = Ay[i+1] - Ay[i]
+    for i in range(0, len(Ay_clean)-1):
+        size = Ay_clean[i+1] - Ay_clean[i]
         if size == 0.:
             size = math.pow(10., -precision)
         if size < minimum:
             minimum = size
             minimumIndex = i
 
-    #print(minimumIndex,minimum)
+    print(minimumIndex,minimum, "minimum")
 
     #then determine from that minimum point to the left-side bin edges
-    newBinEdgeList = [0] * len(Ay)
-    newBinEdgeList[minimumIndex] = Ay[minimumIndex]
+    newBinEdgeList = [0] * len(Ay_clean)
+    newBinEdgeList[minimumIndex] = Ay_clean[minimumIndex]
     currentSize = minimum
     for i in range(minimumIndex-1, 0, -1):
-        size = newBinEdgeList[i+1] - Ay[i]
+        size = newBinEdgeList[i+1] - Ay_clean[i]
         if size < currentSize:
             size = currentSize
         elif size > currentSize:
@@ -56,14 +74,14 @@ def determineQuantiles(hist, nStatMin, precision):
 
         newBinEdgeList[i] = round(newBinEdgeList[i+1] - size, precision)
 
-    newBinEdgeList[0] = Ay[0]
+    newBinEdgeList[0] = Ay_clean[0]
 
-    #print(newBinEdgeList)
+    print(newBinEdgeList, "after left side operation")
 
     #determine the right-side bin edges
     currentSize = minimum
-    for i in range(minimumIndex+1, len(Ay)-1):
-        size = Ay[i] - newBinEdgeList[i-1]
+    for i in range(minimumIndex+1, len(Ay_clean)-1):
+        size = Ay_clean[i] - newBinEdgeList[i-1]
         if size < currentSize:
             size = currentSize
         elif size > currentSize:
@@ -71,11 +89,30 @@ def determineQuantiles(hist, nStatMin, precision):
 
         newBinEdgeList[i] = round(newBinEdgeList[i-1] + size, precision)
 
-    newBinEdgeList[len(Ay)-1] = Ay[len(Ay)-1]
+    newBinEdgeList[len(Ay_clean)-1] = Ay_clean[len(Ay_clean)-1]
 
-   # print(newBinEdgeList)
+    print("newBinEdgeList",newBinEdgeList)
 
-    return newBinEdgeList
+    #do another run over the weighted histograms to find the equivalent weighted bin contents and ensure at least 10 predicted weighted events
+    SecondNewBinEdgeList = [newBinEdgeList[0]]
+    FoundSolutionList = []
+    step = -1
+    for i in range(0, len(newBinEdgeList)-1):
+        if i < step:
+            continue
+        for j in range(i+1, len(newBinEdgeList)):
+            if wHist.Integral(wHist.FindBin(newBinEdgeList[i]), wHist.FindBin(newBinEdgeList[j])-1) > 9.: #3 sigma stat limit with respect to 0
+                SecondNewBinEdgeList.append(newBinEdgeList[j])
+                FoundSolutionList.append(True)
+                step = j
+                break
+
+    if len(SecondNewBinEdgeList)-1 != len(FoundSolutionList): #case that the last bin on the right edge is too small
+        SecondNewBinEdgeList[len(SecondNewBinEdgeList-1)] = newBinEdgeList[len(newBinEdgeList)-1]
+
+    print("SecondNewBinEdgeList",SecondNewBinEdgeList)
+
+    return SecondNewBinEdgeList
 
 
 #This is a small macro to determine a binning with the following requirements:
@@ -93,19 +130,22 @@ fLatex = open("BinTables.tex", "w")
 fBins  = open("BinTables.C"  , "w")
 fBins.write("#include <TROOT.h>\n")
 
-#Macro loads all ttbar events in desired region (electron variant, lower stats)
-chainTtbar = TChain("t")
-chainTtbar.Add("/eos/cms/store/group/phys_b2g/wprime/analyzednewskims/2016_ttbar_semileptonic/*.root")
-chainTtbar.Add("/eos/cms/store/group/phys_b2g/wprime/analyzednewskims/2016_ttbar_leptonic/*.root")
-chainTtbar.Add("/eos/cms/store/group/phys_b2g/wprime/analyzednewskims/2016_ttbar_hadronic/*.root")
-
-#activate necessary branches only, for speed
-chainTtbar.SetBranchStatus("*",0)
-chainTtbar.SetBranchStatus("EventWeight",1)
-chainTtbar.SetBranchStatus("RegionIdentifier",1)
-chainTtbar.SetBranchStatus("JetPt",1)
-chainTtbar.SetBranchStatus("Best_WPrimeMass_*",1)
-chainTtbar.SetBranchStatus("Best_Likelihood_*",1)
+#definition of 2016 ttbar samples
+#ttbarSamples = [["ttbar_semileptonic"      , 366300., 144722000.],
+#                ["ttbar_leptonic"          ,  88500.,  43546000.],
+#                ["ttbar_hadronic"          , 378900., 107067000.],
+#                ["ttbar_bbbar_semileptonic",  19200.,   3223118.],
+#                ["ttbar_bbbar_leptonic"    ,   4600.,   1571799.],
+#                ["ttbar_bbbar_hadronic"    ,  19900.,   2597868.]]
+#2017 ttbar samples
+ttbarSamples = [["ttbar_semileptonic"      , 366300., 346052000.],
+                ["ttbar_leptonic"          ,  88500., 106724000.],
+                ["ttbar_hadronic"          , 378900., 232999999.],
+                ["ttbar_bbbar_semileptonic",  19200.,   7318891.],
+                ["ttbar_bbbar_leptonic"    ,   4600.,   3472503.],
+                ["ttbar_bbbar_hadronic"    ,  19900.,   5694656.]]
+#lumi = 16.81 #2016
+lumi = 41.48 #2017
 
 #declare MC histograms in very fine binning for binning determination
 HThist53  = TH1F("HThist53" ,"", 2000, 0., 2000.)
@@ -127,60 +167,114 @@ for i in range(3,12):
     NLLhists64.append(TH1F("NLLhist64_"+str(i),"", 300,   0., 30.))
     FitHists64.append(TH1F("FitHist64_"+str(i),"", 2000,  0., 2000.))
 
-nentriesMC = chainTtbar.GetEntries()
+#declare weighted MC histograms in very fine binning for binning determination
+wHThist53  = TH1F("wHThist53" ,"", 2000, 0., 2000.)
+wHThist63  = TH1F("wHThist63" ,"", 2000, 0., 2000.)
+wHThist64  = TH1F("wHThist64" ,"", 2000, 0., 2000.)
 
-print("Starting loop over",nentriesMC,"events in MC files")
+wNLLhists53 = []
+wFitHists53 = []
+wNLLhists63 = []
+wFitHists63 = []
+wNLLhists64 = []
+wFitHists64 = []
 
-#loop over MC events in ttbar samples
-for entry in range(0, nentriesMC):
-    chainTtbar.GetEntry(entry)
-    evWeight = getattr(chainTtbar,"EventWeight")
-    regId    = getattr(chainTtbar,"RegionIdentifier")
-    #check for a valid event
-    if evWeight[0] > 0. :
-        #check whether the event is in any region of interest
-        if regId[0] == 2153 or regId[0] == 2164 or regId[0] == 2163:
-            isValidInAllFits = True
-            #select events which are valid for all mass variants, only (acceptance loss is percent level individually, so this is worst case)
-            for i in range(3,12):
-                LL   = getattr(chainTtbar,"Best_Likelihood_"+str(i*100))
-                try:
-                    if -math.log(LL[0]) < 0:
+for i in range(3,12):
+    wNLLhists53.append(TH1F("wNLLhist53_"+str(i),"", 300,   0., 30.))
+    wFitHists53.append(TH1F("wFitHist53_"+str(i),"", 2000,  0., 2000.))
+    wNLLhists63.append(TH1F("wNLLhist63_"+str(i),"", 300,   0., 30.))
+    wFitHists63.append(TH1F("wFitHist63_"+str(i),"", 2000,  0., 2000.))
+    wNLLhists64.append(TH1F("wNLLhist64_"+str(i),"", 300,   0., 30.))
+    wFitHists64.append(TH1F("wFitHist64_"+str(i),"", 2000,  0., 2000.))
+
+for sample in ttbarSamples:
+    #Macro loads all ttbar events in desired region (electron variant, lower stats)
+    chainTtbar = TChain("t")
+    chainTtbar.Add("/eos/cms/store/group/phys_b2g/wprime/analyzeroutputjune2025/2017_"+sample[0]+"/*.root")
+
+    #activate necessary branches only, for speed
+    chainTtbar.SetBranchStatus("*",0)
+    chainTtbar.SetBranchStatus("EventWeight",1)
+    chainTtbar.SetBranchStatus("RegionIdentifier",1)
+    chainTtbar.SetBranchStatus("JetPt",1)
+    chainTtbar.SetBranchStatus("Best_WPrimeMass_*",1)
+    chainTtbar.SetBranchStatus("Best_Likelihood_*",1)
+
+    nentriesMC = chainTtbar.GetEntries()
+
+    print("Starting loop over",nentriesMC,"events in MC file",sample[0])
+
+    MCweight = sample[1]/sample[2]*lumi
+
+    #loop over MC events in ttbar samples
+    for entry in range(0, nentriesMC):
+        chainTtbar.GetEntry(entry)
+        evWeight = getattr(chainTtbar,"EventWeight")
+        regId    = getattr(chainTtbar,"RegionIdentifier")
+        #check for a valid event
+        if evWeight[0] > 0. :
+            #check whether the event is in any region of interest
+            if regId[0] == 2153 or regId[0] == 2164 or regId[0] == 2163:
+                isValidInAllFits = True
+                #select events which are valid for all mass variants, only (acceptance loss is percent level individually, so this is worst case)
+                for i in range(3,12):
+                    LL   = getattr(chainTtbar,"Best_Likelihood_"+str(i*100))
+                    try:
+                        if LL[0] < 0. or LL[0] >= 1.:
+                            isValidInAllFits = False
+                            break
+                    except:
                         isValidInAllFits = False
                         break
-                except:
-                    isValidInAllFits = False
-                    break
 
-            #fill MC fine-binned histograms for the correct regions
-            if isValidInAllFits:
-                JetPt = getattr(chainTtbar,"JetPt")
-                if   regId[0] == 2153:
-                    HThist53.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4])
-                elif regId[0] == 2163:
-                    HThist63.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5])
-                elif regId[0] == 2164:
-                    HThist64.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5])
-
-                for i in range(3,12):
-                    Mfit = getattr(chainTtbar,"Best_WPrimeMass_"+str(i*100))
-                    LL   = getattr(chainTtbar,"Best_Likelihood_"+str(i*100))
+                #fill MC fine-binned histograms for the correct regions
+                if isValidInAllFits:
+                    JetPt = getattr(chainTtbar,"JetPt")
+                    LL500 = getattr(chainTtbar,"Best_Likelihood_"+str(500))
                     if   regId[0] == 2153:
-                        FitHists53[i-3].Fill(Mfit[0])
-                        NLLhists53[i-3].Fill(-math.log(LL[0]))
+                        if -math.log(LL500[0]) > NLLlimits53_500[Optimization[2][0][0]+1] and -math.log(LL500[0]) < NLLlimits53_500[Optimization[2][0][1]+1]:
+                            HThist53.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4])
+                            wHThist53.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4], evWeight[0]*MCweight)
                     elif regId[0] == 2163:
-                        FitHists63[i-3].Fill(Mfit[0])
-                        NLLhists63[i-3].Fill(-math.log(LL[0]))
+                        if -math.log(LL500[0]) > NLLlimits63_500[Optimization[2][1][0]+1] and -math.log(LL500[0]) < NLLlimits63_500[Optimization[2][1][1]+1]:
+                            HThist63.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5])
+                            wHThist63.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5], evWeight[0]*MCweight)
                     elif regId[0] == 2164:
-                        FitHists64[i-3].Fill(Mfit[0])
-                        NLLhists64[i-3].Fill(-math.log(LL[0]))
+                        if -math.log(LL500[0]) > NLLlimits64_500[Optimization[2][1][0]+1] and -math.log(LL500[0]) < NLLlimits64_500[Optimization[2][1][1]+1]:
+                            HThist64.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5])
+                            wHThist64.Fill(JetPt[0]+JetPt[1]+JetPt[2]+JetPt[3]+JetPt[4]+JetPt[5], evWeight[0]*MCweight)
+
+                    for i in range(3,12):
+                        Mfit = getattr(chainTtbar,"Best_WPrimeMass_"+str(i*100))
+                        LL   = getattr(chainTtbar,"Best_Likelihood_"+str(i*100))
+                        if -math.log(LL[0]) < 0.:
+                            print(LL[0], "less than zero NLL")
+                            continue
+                        if   regId[0] == 2153:
+                            if -math.log(LL500[0]) < NLLlimits53_500[Optimization[2][0][0]+1]:
+                                FitHists53[i-3].Fill(Mfit[0])
+                                wFitHists53[i-3].Fill(Mfit[0], evWeight[0]*MCweight)
+                            NLLhists53[i-3].Fill(-math.log(LL[0]))
+                            wNLLhists53[i-3].Fill(-math.log(LL[0]), evWeight[0]*MCweight)
+                        elif regId[0] == 2163:
+                            if -math.log(LL500[0]) < NLLlimits63_500[Optimization[2][1][0]+1]:
+                                FitHists63[i-3].Fill(Mfit[0])
+                                wFitHists63[i-3].Fill(Mfit[0], evWeight[0]*MCweight)
+                            NLLhists63[i-3].Fill(-math.log(LL[0]))
+                            wNLLhists63[i-3].Fill(-math.log(LL[0]), evWeight[0]*MCweight)
+                        elif regId[0] == 2164:
+                            if -math.log(LL500[0]) < NLLlimits64_500[Optimization[2][1][0]+1]:
+                                FitHists64[i-3].Fill(Mfit[0])
+                                wFitHists64[i-3].Fill(Mfit[0], evWeight[0]*MCweight)
+                            NLLhists64[i-3].Fill(-math.log(LL[0]))
+                            wNLLhists64[i-3].Fill(-math.log(LL[0]), evWeight[0]*MCweight)
 
 #Get the quantiles and print
 print("Reached quantile determination stage for MC histograms")
 
-HTbins53 = determineQuantiles(HThist53, nStatMinMC5, 0)
-HTbins63 = determineQuantiles(HThist63, nStatMinMC63, 0)
-HTbins64 = determineQuantiles(HThist64, nStatMinMC64, 0)
+HTbins53 = determineQuantiles(HThist53, nStatMinMC5,  0, wHThist53)
+HTbins63 = determineQuantiles(HThist63, nStatMinMC63, 0, wHThist63)
+HTbins64 = determineQuantiles(HThist64, nStatMinMC64, 0, wHThist64)
 
 FitBins53 = []
 FitBins63 = []
@@ -191,13 +285,13 @@ NLLbins63 = []
 NLLbins64 = []
 
 for i in range(3,12):
-    FitBins53.append(determineQuantiles(FitHists53[i-3], nStatMinMC5, 0))
-    FitBins63.append(determineQuantiles(FitHists63[i-3], nStatMinMC63, 0))
-    FitBins64.append(determineQuantiles(FitHists64[i-3], nStatMinMC64, 0))
+    FitBins53.append(determineQuantiles(FitHists53[i-3], nStatMinMC5,  0, wFitHists53[i-3]))
+    FitBins63.append(determineQuantiles(FitHists63[i-3], nStatMinMC63, 0, wFitHists63[i-3]))
+    FitBins64.append(determineQuantiles(FitHists64[i-3], nStatMinMC64, 0, wFitHists64[i-3]))
 
-    NLLbins53.append(determineQuantiles(NLLhists53[i-3], nStatMinMC5, 1))
-    NLLbins63.append(determineQuantiles(NLLhists63[i-3], nStatMinMC63, 1))
-    NLLbins64.append(determineQuantiles(NLLhists64[i-3], nStatMinMC64, 1))
+    NLLbins53.append(determineQuantiles(NLLhists53[i-3], nStatMinMC5,  1, wNLLhists53[i-3]))
+    NLLbins63.append(determineQuantiles(NLLhists63[i-3], nStatMinMC63, 1, wNLLhists63[i-3]))
+    NLLbins64.append(determineQuantiles(NLLhists64[i-3], nStatMinMC64, 1, wNLLhists64[i-3]))
 
 #write the tables
 fLatex.write("5 jets 3 \PQb tags \HT bin edges:\n")
@@ -339,7 +433,7 @@ for i in range(3,12):
 
 #Macro loads all data events in desired control region for ST binning (electron variant, lower stats)
 chainData = TChain("t")
-chainData.Add("/eos/cms/store/group/phys_b2g/wprime/analyzednewskims/2016_SingleElectron/*.root")
+chainData.Add("/eos/cms/store/group/phys_b2g/wprime/analyzeroutputjune2025/2016_SingleElectron/*.root")
 chainData.SetBranchStatus("*",0)
 chainData.SetBranchStatus("EventWeight",1)
 chainData.SetBranchStatus("RegionIdentifier",1)
@@ -348,8 +442,8 @@ chainData.SetBranchStatus("Best_Likelihood_*",1)
 chainData.SetBranchStatus("LeptonPt",1)
 chainData.SetBranchStatus("METPt",1)
 
-SThist5 = TH1F("SThist5","", 1820, 180., 2000.)
-SThist6 = TH1F("SThist6","", 1790, 210., 2000.)
+SThist5 = TH1F("SThist5","", 1600, 400., 2000.)
+SThist6 = TH1F("SThist6","", 1600, 400., 2000.)
 
 nentriesData = chainData.GetEntries()
 
@@ -380,8 +474,8 @@ for entry in range(0, nentriesData):
 #Get the quantiles
 print("Reached quantile determination stage for ST histograms")
 
-STbins5 = determineQuantiles(SThist5, nStatMinST5, 0)
-STbins6 = determineQuantiles(SThist6, nStatMinST6, 0)
+STbins5 = determineQuantiles(SThist5, nStatMinST5, 0, SThist5)
+STbins6 = determineQuantiles(SThist6, nStatMinST6, 0, SThist6)
 
 #write the tables
 fLatex.write("5 jets 2 \PQb tags $S_{\textrm{T}}$ bin edges:\n")
