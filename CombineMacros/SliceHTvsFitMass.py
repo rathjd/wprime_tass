72d4e032c76eff74df2cbaecc480c4ab563b5e1d
Denis Rathjens <Denis.Rathjens@Cern.ch>
2026-01-27 18:34:28 +0100

    slicing now includes QCD correctly

--- CombineMacros/SliceHTvsFitMass.py
+++ CombineMacros/SliceHTvsFitMass.py
@@ -74,10 +74,11 @@ except:
 NLLvals = [["ttbar",      0., 0., 1., 1.],
            ["wjets",      0., 0., 1., 1.],
            ["single_top", 0., 0., 1., 1.],
-           ["diboson",    0., 0., 1., 1.]]
+           ["diboson",    0., 0., 1., 1.],
+           ["qcd",        0., 0., 1., 1.]]
 
-if year == "2018":
-    NLLvals.append(["qcd", 0., 0., 1., 1.])
+#if year == "2018":
+#    NLLvals.append(["qcd", 0., 0., 1., 1.])
 
 HTnorm = 0
 FitNorm = 0
