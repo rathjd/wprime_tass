91e51bc1bc9439619326c578646d3f7a2ed69dfe
Denis Rathjens <Denis.Rathjens@Cern.ch>
2026-02-05 19:45:13 +0100

    removed 6j4b split testing due to using unsliced HT for that now

--- CombineMacros/SubmitAllSplits.py
+++ CombineMacros/SubmitAllSplits.py
@@ -6,7 +6,7 @@ for year in yearList:
     for lept in range(1,3):
         for jet in range(5,7):
             for btag in range(3,5):
-                if jet == 5 and btag == 4:
+                if btag == 4:
                     continue
                 print("condor_submit SubmitSplit"+str(lept)+"1"+str(jet)+str(btag)+"_"+year+".sub")
                 os.system("condor_submit SubmitSplit"+str(lept)+"1"+str(jet)+str(btag)+"_"+year+".sub")
